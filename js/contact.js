e54c85a0549db6cbbbea8fbc5e8f8bf07c7a9de8
jasonpyau <jasonyau101@gmail.com>
2024-09-15 14:25:31 -0400

    Functional Retrieve Contacts javascript

--- js/contact.js
+++ js/contact.js
@@ -3,6 +3,10 @@
 const urlBase = '/LAMPAPI';
 const extension = 'php';
 
+document.addEventListener('DOMContentLoaded', () => {
+    retrieveContacts();
+});
+
 function readCookie()
 {
 	let returnObject = {};
@@ -100,15 +104,88 @@ function createContact() {
 	}
 }
 
-function updateContact() {
-
+function updateContact(e) {
+    const ContactID = e.value;
+    // Get the ContactID to update!
+    console.log(ContactID)
 }
 
 function retrieveContacts() {
-	// TODO
+    const UserID = readCookie().userId;
+    const search = document.getElementsByClassName("search-bar")[0].querySelector("input").value;
+    console.log(search);
+    let toBeSent = {UserID:UserID, search:search};
+    let jsonToBeSent = JSON.stringify(toBeSent);
+    let url = urlBase + '/RetrieveContacts.' + extension;
+
+    // SET UP HTTP REQUEST
+	let xhr = new XMLHttpRequest();
+    // HTML method, URL, asynchronous or not
+	// true allows for browser to execute other code in the meantime
+	// POST request
+	xhr.open("POST", url, true);
+    // Content-type is the name of the HTTP header being set
+	// Says body contains JSON and is encoded with UTF-8
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try {
+        // HANDLE RESPONSE
+		// onreadystatechange is an event handler, executes function whenever readyState changes
+		xhr.onreadystatechange = function() 
+		{
+			// readyState 4 means complete. Status 200 means successful
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				// Convert JSON string to JS object
+				let jsonObject = JSON.parse( xhr.responseText ); //Response data
+				if(jsonObject.error)
+				{		
+					alert("Error in loading contacts!")
+					return;
+				}
+
+                // Display the contacts.
+                displayContacts(jsonObject);
+			}
+		};
+        // SEND REQUEST
+		xhr.send(jsonToBeSent);
+		// Between sending the request and hearing back, the server determines if the login is successful
+		// See Login.php
+    }
+    catch(err)
+	{
+		document.getElementById("signup-password-error").innerText = err.message;
+	}
 }
 
-function deleteContact() {
+function displayContacts(contactsJson) {
+    const contactListBody = document.getElementById("contact-list-body");
+    const contactRowTemplate = contactListBody.querySelector("#contact-row");
+    contactListBody.innerHTML = "";
+    contactListBody.appendChild(contactRowTemplate);
+    contactRowTemplate.style.display = 'none';
+    // console.log(contactsJson);
+    for (const contact of contactsJson.Contacts) {
+        const currContactRow = contactRowTemplate.cloneNode(true);
+        currContactRow.querySelector("#contact-name").innerText = `${contact.firstName} ${contact.lastName}`;
+        // const phoneNumber = `(${contact.phoneNumber.substring(0,3)}) ` +
+        //                     `${contact.phoneNumber.substring(3,6)}` +
+        //                     `-${contact.phoneNumber.substring(6,10)}`;
+        currContactRow.querySelector("#contact-phone").innerText = contact.phoneNumber;
+        currContactRow.querySelector("#contact-email").innerText = contact.email;
+        currContactRow.querySelector("#edit-contact").value = contact.ContactID;
+        currContactRow.querySelector("#delete-contact").value = contact.ContactID;
+        currContactRow.style.display = 'inline';
+        contactListBody.appendChild(currContactRow);
+        // console.log(currContactRow);
+    }
+}
+
+function deleteContact(e) {
+    // Get the ContactID to delete!
+    const ContactID = e.value;
+    console.log(ContactID);
 	// Delete button: should just click it, ask for confirmation, then delete
 
 	// Read the info of the contact to delete
